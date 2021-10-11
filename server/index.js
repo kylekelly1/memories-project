469562353732ef7bf37182ff09a1da8d9c669bbc
Kyle Kelly <kkelly@belltec.com>
2021-10-11 15:08:35 -0500

    project completed

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 
 //body-parser deprecated 
@@ -16,10 +18,13 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://kylekelly:kylekelly123@cluster0.rautm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+app.get('/', (req, res) => {
+    res.send('Hello to memories API');
+});
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
